77fddbc609a18fb250603a8808b76efb9a06f5b0
cpmann <camillamann@gmail.com>
2018-08-07 12:28:41 -0700

    Updated termination condition, accuracy checking
    Changed minimum diversity, calculated and plotted accuracy

--- termination_condition.m
+++ termination_condition.m
@@ -1,12 +1,14 @@
 % Termination operator **UNFINISHED**
 
 function [result, terminate] = termination_condition(population, fitness, prev_gen_result, delta)
-    min_diversity = 2;
+    min_diversity = 230;
     diversity = sum(pdist(population, 'hamming'));
     mean_fitness = mean(fitness);
     max_fitness = max(fitness);
     result = [diversity, mean_fitness, max_fitness];
+    %if((abs(result(2)-prev_gen_result(2))<= delta) & (diversity <= min_diversity)) 
     if((abs(result-prev_gen_result)<= delta) & (diversity <= min_diversity)) 
+    %if((abs(result-prev_gen_result)<= delta))
         terminate = true;
     else
         terminate = false;
